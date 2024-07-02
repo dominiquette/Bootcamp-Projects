6f1adbcc576c1e79466e6f5fa0103d198dfe5456
Dominique Pacquette <119539825+dominiquette@users.noreply.github.com>
2024-07-02 17:59:18 +0100

    Delete Assignment_4 directory
    Error. added on the wrong branch

--- Assignment_4/config.py
+++
@@ -1,5 +0,0 @@
-# Config file with database connection details
-USER = 'root'  # Replace with your MySQL username
-HOST = 'localhost'  # Replace with your MySQL host
-PASSWORD = 'PassWord'  # replace with your MySQL password
-DATABASE = 'Kids_Party_Booking'  # Database name
